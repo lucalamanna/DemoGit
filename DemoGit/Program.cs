d70f3af10c9c13f813e1c7e18e798dba4dfdf1cd
lucalamanna <lucalamanna2019@gmail.com>
2026-03-24 17:36:58 -0300

    Agrego suma y multiplicación

--- DemoGit/Program.cs
+++ DemoGit/Program.cs
@@ -4,4 +4,16 @@
     {
         Console.WriteLine("Hello, World!");
     }
+
+    public static void Sumar(double a, double b)
+    {
+        double resultado = a + b;
+        Console.WriteLine($"Suma: {a} + {b} = {resultado}");
+    }
+
+    public static void Multiplicar(double a, double b)
+    {
+        double resultado = a * b;
+        Console.WriteLine($"Multiplicación: {a} * {b} = {resultado}");
+    }
 }
